aa9233b524f277a4d45d521b43abebeed7920e02
PedroGranados <pedrog19@tamu.edu>
2025-02-13 09:41:44 -0600

    hi lab 2 :)

---
+++ Untitled-1.js
@@ -0,0 +1,2 @@
+var house ={numberOfRooms:"4",77777 price:"100k", yearbuild:"2005"}
+print( "This house has" + house.numberOfRooms+ "rooms");
---
+++ var MyAge = 22.java
@@ -0,0 +1,13 @@
+var MyAge = 22
+var MyAgeInDays= 'MyAge'*365
+var years = *365
+var Mercury = 88
+var Venus = 225
+var Earth = 365.25
+var Mars = 687
+var Jupiter = 11.8 years
+var Saturn = 29.4 years
+var Uranus = 84 years
+var Neptune = 164 years
+var Pluto = 248 years
+"document.getElementById("M").innerHTML=......."
